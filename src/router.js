7e17083f42d86393e01987ee8f1f5358d479c612
裴景琪 <879273795@qq.com>
2019-09-01 20:43:45 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/router.js
+++ src/router.js
@@ -1,23 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+import Home from './views/home'
+import Login from './views/login/index.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
+  routes: [{
+      path: '/login',
+      component: Login
+    },
     {
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
